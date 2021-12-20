5d8b1072a986b4d4008799d8ab91a526ae6e65f9
Jane Jeon <me@janejeon.dev>
2021-12-20 01:12:35 -0500

    add node notifier

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 process.env.JEST_JUNIT_OUTPUT_DIR = 'reports/jest'
 
 module.exports = {
-  reporters: ['default', 'jest-junit']
+  reporters: ['default', 'jest-junit'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80
+  //   }
+  // },
+  errorOnDeprecated: true,
+  notify: true
 }
